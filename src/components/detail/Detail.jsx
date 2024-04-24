1cdd20ba4e02e60607fa2340ed3cbf9f2bbe289a
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-04-24 03:04:28 -0400

    cleaned up details

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,11 +3,17 @@ import { useChatStore } from "../../lib/chatStore";
 import { auth, db } from "../../lib/firebase";
 import { useUserStore } from "../../lib/userStore";
 import "./detail.css";
+import { useState } from "react";
 
 const Detail = () => {
   const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock } =
     useChatStore();
   const { currentUser } = useUserStore();
+  const [isChatSettingsExpanded, setChatSettingsExpanded] = useState(false);
+  const [isPrivacyExpanded, setPrivacyExpanded] = useState(false);
+  const [isSharedPhotosExpanded, setSharedPhotosExpanded] = useState(false);
+  const [isSharedFilesExpanded, setSharedFilesExpanded] = useState(false);
+
   const handleBlock = async () => {
     if (!user) return;
 
@@ -28,67 +34,59 @@ const Detail = () => {
       <div className="user">
         <img src={user?.avatar || ".avatar.png"} alt="" />
         <h2>{user?.username}</h2>
-        <p>Lorem ipsum dolor, sit amet.</p>
+        {/* <p>Lorem ipsum dolor, sit amet.</p> */}
       </div>
       <div className="info">
         <div className="option">
           <div className="title">
             <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
+            <img
+              src={isChatSettingsExpanded ? "./arrowDown.png" : "./arrowUp.png"}
+              alt=""
+              onClick={() => setChatSettingsExpanded(!isChatSettingsExpanded)}
+            />
           </div>
         </div>
         <div className="option">
           <div className="title">
             <span>Privacy & help</span>
-            <img src="./arrowUp.png" alt="" />
+            <img
+              src={isPrivacyExpanded ? "./arrowDown.png" : "./arrowUp.png"}
+              alt=""
+              onClick={() => setPrivacyExpanded(!isPrivacyExpanded)}
+            />
           </div>
         </div>
+
         <div className="option">
           <div className="title">
             <span>Shared Photos</span>
-            <img src="./arrowDown.png" alt="" />
+            <img
+              src={isSharedPhotosExpanded ? "./arrowDown.png" : "./arrowUp.png"}
+              alt=""
+              onClick={() => setSharedPhotosExpanded(!isSharedPhotosExpanded)}
+            />
           </div>
-          <div className="photos">
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="" alt="photo img" />
-                <span>photo_2024_2.pg</span>
+          {isSharedPhotosExpanded && (
+            <div className="photos">
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img src="" alt="photo img" />
+                  <span>photo_2024_2.pg</span>
+                </div>
+                <img src="./download.png" alt="" className="icon" />
               </div>
-              <img src="./download.png" alt="" className="icon" />
             </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="" alt="photo img" />
-                <span>photo_2024_2.pg</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="" alt="photo img" />
-                <span>photo_2024_2.pg</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img src="" alt="photo img" />
-                <span>photo_2024_2.pg</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-          </div>
+          )}
         </div>
         <div className="option">
           <div className="title">
             <span>Shared Files</span>
-            <img src="./arrowUp.png" alt="" />
+            <img
+              src={isSharedFilesExpanded ? "./arrowDown.png" : "./arrowUp.png"}
+              alt=""
+              onClick={() => setSharedFilesExpanded(!isSharedFilesExpanded)}
+            />
           </div>
         </div>
       </div>
