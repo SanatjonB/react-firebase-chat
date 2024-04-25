219fa281afed9dba76b46436bc4d55fd97049782
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-04-25 17:03:18 -0400

    env fixed

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+console.log(import.meta.env.VITE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore();
