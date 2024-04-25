fe14374bd049a9c2c9b5e974bf2f712d5a201605
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-04-25 16:05:30 -0400

    envfileFixed

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AuthDomain,
-  projectId: import.meta.env.VITE_ProjectId,
-  storageBucket: import.meta.env.VITE_StorageBucket,
-  messagingSenderId: import.meta.env.VITE_MessagingSenderId,
-  appId: import.meta.env.VITE_AppId,
-  measurementId: import.meta.env.VITE_MeasurementId,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
